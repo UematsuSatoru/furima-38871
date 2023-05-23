d9dac3fcc57eedf766134285f83850f80d2d7414
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-05-23 22:53:21 +0900

    マイグレーション　修正

--- db/migrate/20230522120647_create_items.rb
+++ db/migrate/20230522120647_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :delivery_charge_id, null: false
       t.integer :number_id, null: false
       t.integer :price, null: false
-      t.references :user null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
     end
   end
 end
