4d7182a2f0a056a7098f547f270605a4e9706569
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-05-25 23:17:19 +0900

    バリデーションを設定

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,4 +1,9 @@
 class Information < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :area, :delivery_charge, :number
+
+  validates :Item_name, :Item_description, presence: true
+
+  validates :category_id, :condition_id, :area_id, :delivery_charge_id, :number_id,  numericality: { other_than: 1 } 
+
 end
