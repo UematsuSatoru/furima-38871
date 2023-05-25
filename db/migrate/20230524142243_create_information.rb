7e97c5a2729744a6bde3bdd00b76e5b3f9fa7074
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-05-25 23:12:25 +0900

    マイグレーションファイルを編集

--- db/migrate/20230524142243_create_information.rb
+++ db/migrate/20230524142243_create_information.rb
@@ -1,7 +1,8 @@
 class CreateInformation < ActiveRecord::Migration[6.0]
   def change
     create_table :information do |t|
-
+      t.text  :Item_name, null: false
+      t.text  :Item_description, null: false
       t.timestamps
     end
   end
