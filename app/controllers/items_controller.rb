1def5d9d8bcdd7b47e0c62d5d9c3fc66431063aa
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-05-31 00:00:16 +0900

    new.html.erbを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(article_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:name,:explanation,:category_id,:condition_id,:area_id,:delivery_charge_id,:number_id,:price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :area, :delivery_charge, :number
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :area
+  belongs_to :delivery_charge
+  belongs_to :number
+  belongs_to :user
 
   validates :Item_name, :Item_description, presence: true
 
   validates :category_id, :condition_id, :area_id, :delivery_charge_id, :number_id,  numericality: { other_than: 1 , message: "can't be blank"} 
-
-  belongs_to :user
 
   has_one :record
   
