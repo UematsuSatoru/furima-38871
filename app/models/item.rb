6e16a1c761868e687991365170da043fb186f1a4
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-06-04 00:53:51 +0900

    バリデーション　編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,11 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :image, :category_id, :condition_id, :area_id, :delivery_charge_id, :number_id, :price, numericality: { other_than: 1 , message: "can't be blank"} 
-
+  validates :image, presence: true
+  validates :category_id, :condition_id, :area_id, :delivery_charge_id, :number_id, :price, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'Price is invalid. Input half-width characters' }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Price is out of setting range' }
+  
   has_one :record
   
   has_one_attached :image
