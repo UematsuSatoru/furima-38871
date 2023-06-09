3414f28844c5188cfb6d1c1856179ad1e7db0cdd
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-06-09 15:00:41 +0900

    出品された日時が新しい順に表示される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
