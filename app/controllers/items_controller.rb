f14405952d47992faafc3ddde29455546ae688ce
UematuSatoru <124174732+UematuSatoru@users.noreply.github.com>
2023-06-10 16:23:05 +0900

    authenticate_user!にshowを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
